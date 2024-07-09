547f5efc03d87227c4811c127e9974d2c0794d5e
avi8230 <avi8230@gmail.com>
2024-07-09 20:36:17 +0300

    Basic version of the application

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ Future<void> main() async {
   runApp(const BasicCrudApp());
 }
 
-addQuote() {
+addQuote(String text, String author) {
   final db = FirebaseFirestore.instance;
-  final data = {"text": "quote text", "author": "author name"};
+  final data = {"text": text, "author": author};
   db.collection("quotes").add(data).then((documentSnapshot) =>
       print("Added Data with ID: ${documentSnapshot.id}"));
 }
@@ -25,22 +25,267 @@ class BasicCrudApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Provider Example'),
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
+      body: const QuoteApp(),
+    ));
+  }
+}
+
+class QuoteApp extends StatefulWidget {
+  const QuoteApp({super.key});
+
+  @override
+  _QuoteAppState createState() => _QuoteAppState();
+}
+
+class _QuoteAppState extends State<QuoteApp> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    DisplayQuotes(),
+    AddQuoteForm(),
+    ManageQuotes(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.format_quote),
+            label: 'Quotes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add Quote',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Manage Quotes',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class DisplayQuotes extends StatefulWidget {
+  @override
+  _DisplayQuotesState createState() => _DisplayQuotesState();
+}
+
+class _DisplayQuotesState extends State<DisplayQuotes> {
+  PageController _pageController = PageController();
+  Stream<QuerySnapshot> _quotesStream =
+      FirebaseFirestore.instance.collection('quotes').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _quotesStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return PageView(
+          controller: _pageController,
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['text'],
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    data['author'],
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
+
+class AddQuoteForm extends StatefulWidget {
+  @override
+  _AddQuoteFormState createState() => _AddQuoteFormState();
+}
+
+class _AddQuoteFormState extends State<AddQuoteForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _textController = TextEditingController();
+  final _authorController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                hintText: 'Enter quote text',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-            body: Center(
-                child: Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () => {addQuote()},
-                    child: const Text('Add Quote')),
-                ElevatedButton(
-                    onPressed: () => {}, child: const Text('Get Quote')),
-                ElevatedButton(
-                    onPressed: () => {}, child: const Text('Delete Quote'))
-              ],
-            ))));
+            TextFormField(
+              controller: _authorController,
+              decoration: const InputDecoration(
+                hintText: 'Enter author name',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter an author';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    addQuote(_textController.text, _authorController.text);
+                    _textController.clear();
+                    _authorController.clear();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Quote Added')),
+                    );
+                  }
+                },
+                child: const Text('Add Quote'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ManageQuotes extends StatelessWidget {
+  final Stream<QuerySnapshot> _quotesStream =
+      FirebaseFirestore.instance.collection('quotes').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _quotesStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['text']),
+              subtitle: Text(data['author']),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      _editQuote(
+                          context, document.id, data['text'], data['author']);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      FirebaseFirestore.instance
+                          .collection('quotes')
+                          .doc(document.id)
+                          .delete();
+                    },
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  void _editQuote(BuildContext context, String id, String text, String author) {
+    TextEditingController textController = TextEditingController(text: text);
+    TextEditingController authorController =
+        TextEditingController(text: author);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Quote'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: textController,
+                decoration: InputDecoration(labelText: 'Quote'),
+              ),
+              TextField(
+                controller: authorController,
+                decoration: InputDecoration(labelText: 'Author'),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection('quotes').doc(id).update({
+                  'text': textController.text,
+                  'author': authorController.text,
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
